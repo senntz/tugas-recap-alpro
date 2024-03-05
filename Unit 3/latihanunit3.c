9ceb0c6ab8302de48318ce6046be5c102fa81240
senntz <161669676+senntz@users.noreply.github.com>
2024-03-05 18:48:17 +0700

    Create latihanunit3.c

---
+++ Unit 3/latihanunit3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int masukan;
+
+int main(){
+  scanf("%d/n", &masukan);
+  int arr[masukan];
+  for(int i = 0; i < masukan; i++){
+    scanf("%d", &arr[i]);
+  }
+  for(int i = 0; i < masukan; i++){
+    printf("%d ", arr[i]);
+  }
+}
